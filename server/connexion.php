dba26188cfd1875c1ef40f157e2f5ea08ac0472b
borislukrece <cheloappdev225@gmail.com>
2023-03-14 14:59:00 +0000

    Mon Portfolio

--- server/connexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-//On se connecte à la BDD
-$bdd = new PDO(
-'mysql:host=localhost;
-dbname=id16313957_portfolio;
-charset=utf8', 'id16313957_chelo', '2001Ahotyboris_');
-
-$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
